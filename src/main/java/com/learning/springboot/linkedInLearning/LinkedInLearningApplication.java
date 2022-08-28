c48231165afd9fe619ddd5e2727b17040fb0fbd3
Tarun Kumar - t0k03w3 <Tarun.Kumar1@walmart.com>
2022-08-29 00:14:48 +0530

    first commit

---
+++ src/main/java/com/learning/springboot/linkedInLearning/LinkedInLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.linkedInLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/springboot/linkedInLearning/LinkedInLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.linkedInLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedInLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
